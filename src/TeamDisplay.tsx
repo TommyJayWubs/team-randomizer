9ba8afafc3dce9dccff51f4830da2dd540d0eadb
TommyJayWubs <thomas.james.williams.8@gmail.com>
2022-08-02 15:25:19 +0100

    minor refactor

--- src/TeamDisplay.tsx
+++ src/TeamDisplay.tsx
@@ -2,6 +2,28 @@ import { ChangeEvent, Dispatch } from "react"
 import { Player, PlayersAction } from "./playersReducer"
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 
+type PlayerDisplayProps = {
+  player: Player,
+  playersDispatch: Dispatch<PlayersAction>,
+}
+
+function PlayerDisplay({ player, playersDispatch }: PlayerDisplayProps) {
+  return (
+    <div className="justify-center">
+      <input
+        value={player.name}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          playersDispatch({ type: 'set', id: player.id, name: e.target.value })
+        }} />
+      <button
+        onClick={() => playersDispatch({ type: 'remove', id: player.id })}
+        className='small red'
+      >
+        -
+      </button>
+    </div>)
+}
+
 type TeamDisplayProps = {
   teamNumber: number,
   teamPlayers: Player[],
@@ -15,20 +37,13 @@ export function TeamDisplay({teamNumber, teamPlayers, playersDispatch} : TeamDis
     <div>
       <h3> Team {teamNumber} </h3>
       <div ref={parent}>
-        {teamPlayers.map(player =>
-          <div key={player.id} className="justify-center">
-            <input
-              value={player.name}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                playersDispatch({ type: 'set', id: player.id, name: e.target.value })
-              }} />
-            <button
-              onClick={() => playersDispatch({ type: 'remove', id: player.id })}
-              className='small red'
-            >
-              -
-            </button>
-          </div>)}
+        {teamPlayers.map(player => 
+          <PlayerDisplay 
+            key={player.id} 
+            player={player} 
+            playersDispatch={playersDispatch}
+          />)
+        }
       </div>
     </div>
   )
